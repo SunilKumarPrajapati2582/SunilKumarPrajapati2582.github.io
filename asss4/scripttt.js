8933af69471341998151fb59d8f600b61fd972b2
SunilKumarPrajapati2582 <140237440+SunilKumarPrajapati2582@users.noreply.github.com>
2023-08-27 17:13:16 +0530

    Update scripttt.js

--- asss4/scripttt.js
+++ asss4/scripttt.js
@@ -1,18 +1,18 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[0]="Ravi";
+names[1]="Raaj";
+names[2]="Jhon";
+names[3]="Divakar";
+names[4]="Jhonny";
+names[5]="Kajal";
+names[6]="Tommy";
+names[7]="Peggi";
+names[8]="Maggie";
+names[9]="Tinn";
 
 
 for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+	if(names[i].charAt(0)==='R'|| names[i].charAt(0)==='r'){
         console.log("Goodbye "+ names[i])
 	}
 	else{
